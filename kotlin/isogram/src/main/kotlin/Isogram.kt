c5e55fb780a1efab26c4e09b9e79bc8b345af104
Chaiwa Berian <chaiwa.berian@bantulabtech.com>
2020-08-24 15:37:26 +0200

    Cleanups for Isogram

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -1,10 +1,10 @@
 object Isogram {
    fun isIsogram(input: String): Boolean {
-        val isog = dup@ {
+        val isog = isIso@ {
             input.toLowerCase().toList().distinct().forEach{x -> 
                 if(input.toLowerCase().filter{
                     it == x && it.isLetter()
-                }.count() > 1) return@dup false
+                }.count() > 1) return@isIso false
             }
         true
         }
