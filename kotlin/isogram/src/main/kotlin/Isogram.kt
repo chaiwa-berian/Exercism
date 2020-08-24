795746b566466fbd81410a1513f3ce8de804dab8
Chaiwa Berian <chaiwa.berian@bantulabtech.com>
2020-08-24 15:47:11 +0200

    Performance for Isogram

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -1,7 +1,7 @@
 object Isogram {
    fun isIsogram(input: String): Boolean {
         val isog = isIso@ {
-            input.toLowerCase().toList().distinct().forEach{x -> 
+            input.toLowerCase().toList().distinct().filter{ v -> v.isLetter()}.forEach{x -> 
                 if(input.toLowerCase().filter{
                     it == x && it.isLetter()
                 }.count() > 1) return@isIso false
