52abfbd0df1b0a7ce95eadd518234ef44f6b7dc2
Chaiwa Berian <chaiwa.berian@bantulabtech.com>
2020-08-24 15:34:19 +0200

    Refactor Isogram

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -1,10 +1,13 @@
 object Isogram {
-
-    fun isIsogram(input: String): Boolean {
-        return ('a'..'z').all{x ->
-            input.toLowerCase().filter{
-                it == x
-            }.count() <= 1
+   fun isIsogram(input: String): Boolean {
+        val isog = dup@ {
+            input.toLowerCase().toList().distinct().forEach{x -> 
+                if(input.toLowerCase().filter{
+                    it == x && it.isLetter()
+                }.count() > 1) return@dup false
+            }
+        true
         }
-    }
+        return isog()
+    }  
 }
