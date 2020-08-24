dd00e89f10501b795a87020a267db3bd4d024ed0
Chaiwa Berian <chaiwa.berian@bantulabtech.com>
2020-08-24 12:23:36 +0200

    Refactor Pangram

--- kotlin/pangram/src/main/kotlin/Pangram.kt
+++ kotlin/pangram/src/main/kotlin/Pangram.kt
@@ -1,13 +1,5 @@
 object Pangram {
-
     fun isPangram(input: String): Boolean {
-        val alphabet = "abcdefghijklmnopqrstuvwxyz"
-        val isPang = isPang@ {
-            alphabet.forEach {
-                if(input.indexOf(it, ignoreCase=true) == -1) return@isPang false
-            }
-          true  
-        } 
-        return isPang()
+       return ('a'..'z').all{it in input.toLowerCase()}
     }
 }
